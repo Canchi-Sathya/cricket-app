4079eaf33b3196c19bd5debaff39acbf0006625e
Canchi Sathya <canchisathya@gmail.com>
2026-09-21 09:58:51 -0400

    Use environment variable for API base URL

--- fromtend/src/api/client.js
+++ fromtend/src/api/client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://localhost:8000"});
+const api = axios.create({ baseURL: process.env.REACT_APP_API_URL || "http://localhost:8000" });
 
 export const register = (data) => api.post("/auth/register", data);
 export const login = (data) => api.post("/auth/login", data);
